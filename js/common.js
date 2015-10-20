d6fd7160e846073667498e8797eb153439860517
ex-zy <serega1905@ukr.net>
2015-10-20 17:48:31 +0300

    add last files

--- js/common.js
+++ js/common.js
@@ -33,6 +33,7 @@ head.ready(function() {
 	$('.js-main-btn').on('click', function() {
         $('.faq__drop').toggleClass('is-drop__open');
         $('.is-marker1').toggleClass('is-active');
+        $('.is-open').toggleClass('is-close');
         return false;
     });
 
@@ -40,6 +41,7 @@ head.ready(function() {
     $('.js-second-btn').on('click', function() {
         $('.faq__second-drop').toggleClass('is-drop__open');
         $('.is-marker2').toggleClass('is-active');
+        $('.is-open__drop').toggleClass('is-close');
         return false;
     });
 
